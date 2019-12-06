497a713072c230106a6ec724536b45af04181460
Callum Macdonald <git@callum-macdonald.com>
2019-12-06 19:12:29 +0100

    Save roles to local storage.

--- frontend/src/services/auth/auth.service.ts
+++ frontend/src/services/auth/auth.service.ts
@@ -2,6 +2,7 @@ import Auth0Lock from "auth0-lock";
 
 const TOKEN_STORAGE_KEY = "_authToken";
 const USER_ID_STORAGE_KEY = "_userId";
+const ROLES_STORAGE_KEY = "_roles";
 
 const lock = new Auth0Lock(
   "mZeX1QFQKvmzwjZKYRcvmzYsO8d1Ygox",
@@ -11,6 +12,11 @@ const lock = new Auth0Lock(
 lock.on("authenticated", (authResult: AuthResult) => {
   localStorage.setItem(TOKEN_STORAGE_KEY, authResult.idToken);
   localStorage.setItem(USER_ID_STORAGE_KEY, authResult.idTokenPayload.sub);
+
+  const roles = (authResult.idTokenPayload as any)[
+    "https://hasura.io/jwt/claims"
+  ]["x-hasura-allowed-roles"];
+  localStorage.setItem(ROLES_STORAGE_KEY, JSON.stringify(roles));
 });
 
 export const showLock = () => {
@@ -23,4 +29,13 @@ export const getToken = () => {
 
 export const getUserId = () => {
   return localStorage.getItem(USER_ID_STORAGE_KEY);
+};
+
+export const getRoles = (): string[] => {
+  return JSON.parse(localStorage.getItem(ROLES_STORAGE_KEY) || `["user"]`);
+};
+
+export const userHasRole = (role: string) => {
+  const roles = getRoles();
+  return roles.indexOf(role) !== -1;
 };
