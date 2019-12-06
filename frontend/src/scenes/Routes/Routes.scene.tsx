d49a197710d3b855989cd315bb2c7cea4139b2bb
Callum Macdonald <git@callum-macdonald.com>
2019-12-06 14:26:44 +0100

    Add material-ui theme.

--- frontend/src/scenes/Routes/Routes.scene.tsx
+++ frontend/src/scenes/Routes/Routes.scene.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Auth0Lock from "auth0-lock";
+import {
+  createStyles,
+  withStyles,
+  WithStyles,
+  Theme,
+  createMuiTheme
+} from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import { responsiveFontSizes } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
 
 import Profile from "../Profile/Profile.scene";
+
+const baseTheme = createMuiTheme();
+const theme = responsiveFontSizes(baseTheme);
 
 const lock = new Auth0Lock(
   "mZeX1QFQKvmzwjZKYRcvmzYsO8d1Ygox",
@@ -35,13 +49,27 @@ const Home = () => {
 
 const Routes = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/profile" component={Profile} />
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <CssBaseline />
+        <Container>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/profile" component={Profile} />
+          </Switch>
+        </Container>
+      </Router>
+    </ThemeProvider>
   );
 };
 
-export default Routes;
+const styles = (theme: Theme) =>
+  createStyles({
+    container: {
+      flexGrow: 1
+    }
+  });
+
+type Props = WithStyles<typeof styles>;
+
+export default withStyles(styles)(Routes);
