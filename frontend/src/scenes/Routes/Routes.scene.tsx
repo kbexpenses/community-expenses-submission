e1fa4dda3367e02718063a82b25ac4b2b4ab5b9f
Callum Macdonald <git@callum-macdonald.com>
2019-12-23 22:10:06 +0100

    Redirect to the homepage if you're not logged in.

--- frontend/src/scenes/Routes/Routes.scene.tsx
+++ frontend/src/scenes/Routes/Routes.scene.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 import { connect } from "react-redux";
 import {
   createStyles,
@@ -46,7 +51,10 @@ const Routes = (props: Props) => {
               <Route exact path="/budget" component={BudgetIndex} />
             </Switch>
           ) : (
-            <Home />
+            <>
+              <Redirect to="/" />
+              <Home />
+            </>
           )}
         </Container>
       </Router>
