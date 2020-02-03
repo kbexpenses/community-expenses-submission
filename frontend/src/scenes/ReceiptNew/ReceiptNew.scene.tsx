0c29e897f9c9d3a2c3105308e04e014e1b3370ad
Callum Macdonald <git@callum-macdonald.com>
2020-02-03 21:42:42 +0100

    Clearer label of receipt date.

--- frontend/src/scenes/ReceiptNew/ReceiptNew.scene.tsx
+++ frontend/src/scenes/ReceiptNew/ReceiptNew.scene.tsx
@@ -60,7 +60,8 @@ const formSchema = gql`
 const formSchemaType = buildASTSchema(formSchema).getType("Receipt");
 const formSchemaExtras = {
   date: {
-    type: "Date"
+    type: "Date",
+    label: "Date of receipt"
   },
   amount: {
     label: "Amount of this receipt in EUR"
